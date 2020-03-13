33b4da53b0b8b0408b33fba86038202f684a54cf
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2020-03-13 16:35:17 +0530

    Update __init__.py

--- hospital_app/__init__.py
+++ hospital_app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy                 #SQL database
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+##SQL database
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
