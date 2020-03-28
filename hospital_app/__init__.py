b6cf3410b96c054048a8eee9409670163ec823ee
Sapna20 <2017csb1105@iitrpr.ac.in>
2020-03-29 01:57:37 +0530

    static files added

--- hospital_app/__init__.py
+++ hospital_app/__init__.py
@@ -15,32 +15,24 @@ login = LoginManager(app)
 login.login_view='login'
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
-<<<<<<< Updated upstream
 #user_collection = mongo.db.users               uncomment for testing connection hereonly
 #user_collection.insert({'name':'Anthoy'})
-=======
-# user_collection = mongo.db.users
-# user_collection.insert({'name':'Anthoy'})
->>>>>>> Stashed changes
+
 migrate = Migrate(app, db)
 mail = Mail(app)
 
 from hospital_app import  models
 from .views.login import login_bp
 from .views.register import register_bp
-<<<<<<< Updated upstream
+
 from .views.admin import admin_bp
 app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
 app.register_blueprint(admin_bp)
-=======
-# from .views.doctor_routes import doctor_routes_bp
-from .views.hello import hello
-app.register_blueprint(login_bp)
-app.register_blueprint(register_bp)
-app.register_blueprint(hello)
-# app.register_blueprint(doctor_routes_bp)
->>>>>>> Stashed changes
+
+from .views.doctor_routes import doctor_routes_bp
+# from .views.hello import hello
+app.register_blueprint(doctor_routes_bp)
 
 
     
--- hospital_app/views/doctor_routes.py
+++ hospital_app/views/doctor_routes.py
@@ -1,19 +1,19 @@
-# from flask import Blueprint, render_template, redirect,url_for, request, flash
-# from hospital_app import mongo
-# import json
-# doctor_routes_bp = Blueprint('doctor_routes',__name__)
+from flask import Blueprint, render_template, redirect,url_for, request, flash
+from hospital_app import mongo
+import json
+doctor_routes_bp = Blueprint('doctor_routes',__name__)
 
-# @doctor_routes_bp.route('/doctor')
-# def doctor():       #pass user_id as parameter
-#     return render_template('Doctor/home.html')
+@doctor_routes_bp.route('/doctor')
+def doctor():       #pass user_id as parameter
+    return render_template('Doctor/home.html')
 
-# @doctor_routes_bp.route('/start_treatment', methods=['GET', 'POST'])
-# def start_treatment():       #pass user_id as parameter
+@doctor_routes_bp.route('/start_treatment', methods=['GET', 'POST'])
+def start_treatment():       #pass user_id as parameter
     
-#     if request.method == 'POST':
-#             x =  { "name":"John", "age":30, "city":"New York"}
-#             #mongo.db.Treatment.insert({'Disease name' : request.form['Disease'], 'Doctors involved' : request.form['Doctor']})
-#             mongo.db.Treatment.insert(x)
-#             return redirect(url_for('doctor_routes.doctor'))
+    if request.method == 'POST':
+            x =  { "name":"John", "age":30, "city":"New York"}
+            #mongo.db.Treatment.insert({'Disease name' : request.form['Disease'], 'Doctors involved' : request.form['Doctor']})
+            mongo.db.Treatment.insert(x)
+            return redirect(url_for('doctor_routes.doctor'))
         
-#     return render_template('Doctor/Start_treatment.html')
+    return render_template('Doctor/Start_treatment.html')
