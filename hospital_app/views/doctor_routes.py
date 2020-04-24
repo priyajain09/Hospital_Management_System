4634d723f9c855306962006dd8f0248da8713aa1
Sapna20 <2017csb1105@iitrpr.ac.in>
2020-04-24 16:35:57 +0530

    backend code -  doctor  -  view curr treatment, prescription functionalities

--- hospital_app/views/doctor_routes.py
+++ hospital_app/views/doctor_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect,url_for, request, flash
 from hospital_app import mongo
 import json
+from flask_login import current_user
 # from datetime import date
 from datetime import datetime
 
@@ -44,8 +45,49 @@ def start_treatment():
             mongo.db.Treatment.update(query_total_treat,{"$set":{'total_treatments':bson_int64_treat_id}})
             
             #insert new treatment data
-            mongo.db.Treatment.insert({'treat_id' : bson_int64_treat_id, 'patient_userid' : patient_userid, 'patient_name' : patient_name,'disease_name' : "", 'Referto' : "",'time_stamp': datetime.now(), 'alldoctors':doctor_inputs})
+            mongo.db.Treatment.insert({'treat_id' : bson_int64_treat_id, 'patient_userid' : patient_userid, 'patient_name' : patient_name,'disease_name' : "", 'Referto' : "",'total_prescriptions' : 0,'time_stamp': datetime.now(), 'alldoctors':doctor_inputs})
             
             return redirect(url_for('doctor_routes.home_page'))
         
     return render_template('Doctor/doctor_sites/Start_treatment.html')
+
+@doctor_routes_bp.route('/current_treatment_list', methods=['GET', 'POST'])
+def current_treatment_list():
+    doc_treatment_cursor = mongo.db.Treatment.find({ "treat_id": { "$ne": 0 } })
+    return render_template('Doctor/doctor_sites/current_treatment_list.html',treatment=doc_treatment_cursor)
+    
+@doctor_routes_bp.route('/treatment_info/<treat_id>', methods=['GET', 'POST'])
+def treatment_info(treat_id):
+    doc_treatment = mongo.db.Treatment.find_one({ "treat_id": int(treat_id) })
+    print(doc_treatment)
+    total_prescriptions = doc_treatment['total_prescriptions']
+    print(total_prescriptions)
+    print(type(total_prescriptions))
+    #to be done : editing some attributes of treatment document like disease_name, alldoctors etc.
+    if total_prescriptions == 0 :
+        return render_template('Doctor/doctor_sites/treatment_info.html', treatment=doc_treatment, treat_id = treat_id)
+    else :
+        return render_template('Doctor/doctor_sites/treatment_info.html', treatment=doc_treatment, treat_id = treat_id, prescriptions=doc_treatment["prescription"])
+
+@doctor_routes_bp.route('/add_prescription/<treat_id>', methods=['GET', 'POST'])
+def add_prescription(treat_id):
+    if request.method == 'POST':
+        #data from start treatment form
+        tests = request.form['tests']
+        diet_plan = request.form['diet_plan']
+        str_next_visit_date = request.form['next_visit_date']
+        medicines_inputs = request.form.getlist('medicines_inputs[]')
+        symptoms_inputs = request.form.getlist('symptoms_inputs[]')
+
+        #get and update total prescription under a treatment
+        doc_treatment = mongo.db.Treatment.find({ "treat_id": int(treat_id) })
+        doc_treatment_list = list(doc_treatment)
+        presc_id = doc_treatment_list[0].get('total_prescriptions')+1
+
+        #update total prescriptions number under a treatment
+        mongo.db.Treatment.update({ "treat_id": int(treat_id) },{"$set":{'total_prescriptions':presc_id }})
+        
+        #insert new prescription data
+        mongo.db.Treatment.update({'treat_id' : int(treat_id) },{'$push':{"prescription" :{ 'pres_id' : presc_id , 'pres_doctor_userid' : current_user.username , 'time_stamp': datetime.now(), 'tests' : tests , 'diet_plan' : diet_plan, 'str_next_visit_date' : str_next_visit_date , 'medicines_inputs' : medicines_inputs , 'symptoms_inputs' : symptoms_inputs }}})
+
+    return redirect(url_for('doctor_routes.treatment_info', treat_id = treat_id))
