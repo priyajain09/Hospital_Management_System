1cbdfa0b9c1bda05fcaee0e80c2175c97c6ec602
Sapna20 <2017csb1105@iitrpr.ac.in>
2020-08-08 14:56:53 +0530

    modified

--- hospital_app/views/compounder/comp.py
+++ hospital_app/views/compounder/comp.py
@@ -23,18 +23,16 @@ def queue():
 
 @comp_bp.route('/visit_patient/<username>')
 def visit_patient(username):
-    treatment = mongo.db.Treatment.find({"patient_userid": username })
-    return render_template('Compounder/sites/visit_patient.html', username = username, treatment = treatment)
+    treatments = mongo.db.Treatment.find({"patient_userid": username })
+    return render_template('Compounder/sites/visit_patient.html', username = username, treatments = treatments)
 
-@comp_bp.route('/comp_start_treatment/<username>')
-def comp_start_treatment(username):
+@comp_bp.route('/comp_start_new_treatment/<username>')
+def comp_new_start_treatment(username):
 
     user = Patient.query.filter_by(username = username).first()
     print(user.name)
     #data from start treatment form
 
-    doctor_inputs = []
-    doctor_closed = []
     #get total number of treatments from mongo and increment it by 1 and update it
     query_total_treat = { "Description": "Total number of treatments started" }
     doc_total_treat_cursor = mongo.db.Treatment.find(query_total_treat)
@@ -44,7 +42,48 @@ def comp_start_treatment(username):
     mongo.db.Treatment.update(query_total_treat,{"$set":{'total_treatments':bson_int64_treat_id}})
             
     #insert new treatment data
-    mongo.db.Treatment.insert({'treat_id' : bson_int64_treat_id, 'patient_userid' : username, 'patient_name' : user.name,'disease_name' : "", 'Referto' : "",'total_prescriptions' : 0,'time_stamp': datetime.now(), 'alldoctors':doctor_inputs, 'doctor_closed' : doctor_closed})
+    mongo.db.Treatment.insert({'treat_id' : bson_int64_treat_id, 'patient_userid' : username, 'patient_name' : user.name, 'gender' : user.gender_user, 'age' : user.age , 'blood_group' : user.blood_group,'disease_name' : "", 'Referto' : "",'total_prescriptions' : 0,'time_stamp': datetime.now(), 'doctorid': '', 'allergies': [], 'chronic':[], 'prescription' : []})
     treatment = mongo.db.Treatment.find_one({'treat_id' : bson_int64_treat_id })
     return render_template('Compounder/sites/comp_start_treatment.html', treatment = treatment)
 
+@comp_bp.route('/comp_add_prescription/<treat_id>', methods=['GET', 'POST'])
+def add_prescription(treat_id):
+
+    treatment = mongo.db.Treatment.find_one({'treat_id' : int(treat_id) })
+
+    if request.method == 'POST':
+        temperature = request.form['temperature']
+        Sys = request.form['Sys']
+        Dia = request.form['Dia']
+        allergies = request.form['allergies']
+        allergies = allergies.split()
+        chronic = request.form['chronic']
+        chronic = chronic.split()
+        if treatment['doctorid'] == '':
+            doctorid = request.form['doctor']
+        print(temperature)
+
+        #new treatment
+        if treatment['total_prescriptions'] == 0:
+            mongo.db.Treatment.update({ "treat_id": int(treat_id) },{"$set":{'total_prescriptions': 1 , 'doctorid': doctorid }})
+            mongo.db.Treatment.update({ "treat_id": int(treat_id) },{'$push':{"prescription" :{ 'pres_id' : 1, 'blood_pressure' : Sys+'/'+Dia+'mm Hg' , 'temperature' : temperature } } })
+
+            mongo.db.Treatment.update({ "treat_id": int(treat_id) },{'$push':{ 'allergies' : { '$each': allergies }}})
+            mongo.db.Treatment.update({ "treat_id": int(treat_id) },{'$push':{ 'chronic' : { '$each': chronic }}})
+
+        #existing treatment
+        if treatment['total_prescriptions'] != 0:
+            mongo.db.Treatment.update({ "treat_id": int(treat_id) },{"$set":{'total_prescriptions': treatment['total_prescriptions'] + 1 }})
+            mongo.db.Treatment.update({ "treat_id": int(treat_id) },{'$push':{"prescription" :{ 'pres_id' : treatment['total_prescriptions'] + 1, 'blood_pressure' : Sys+'/'+Dia+' mm Hg' , 'temperature' : temperature } } })
+
+            mongo.db.Treatment.update({ "treat_id": int(treat_id) },{'$push':{ 'allergies' : { '$each': allergies }}})
+            mongo.db.Treatment.update({ "treat_id": int(treat_id) },{'$push':{ 'chronic' : { '$each': chronic }}})
+
+
+    return redirect(url_for('comp.queue'))
+
+@comp_bp.route('/comp_continue_treatment/<treat_id>')
+def comp_continue_treatment(treat_id):
+    treatment = mongo.db.Treatment.find_one({'treat_id' : int(treat_id) })
+
+    return render_template('Compounder/sites/comp_start_treatment.html', treatment = treatment)
