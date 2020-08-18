6bb9f2905fb662e99db9bd32edcf9ad55a7e5749
Sapna20 <2017csb1105@iitrpr.ac.in>
2020-08-18 22:13:40 +0530

    View current -past assisstant added

--- hospital_app/views/doctor/doctor_routes.py
+++ hospital_app/views/doctor/doctor_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect,url_for, request, flash
 from hospital_app import mongo
-from hospital_app.models import User,Doctor , patient_queue, Medicine, Disease, Symptom
+from hospital_app.models import User,Doctor , patient_queue, Medicine, Disease, Symptom, user_role, past_user_role
 from hospital_app import db
 import json
 from flask_login import current_user
@@ -219,3 +219,23 @@ def prescription_history(treat_id):
         return "This Treatment does not exist"
     prescriptions = reversed(treatment['prescription'])
     return render_template('Doctor/doctor_sites/prescription_history.html', prescriptions = prescriptions, treatment = treatment)    
+
+@doctor_routes_bp.route('/doctor/current_assistant')
+def current_assistant():
+    u = user_role.query.filter_by(role = "assistant",doctor_username = current_user.username ).first()
+    if u is not None:
+        image = base64.b64encode(u.File).decode('ascii')
+        return render_template('Doctor/doctor_sites/current_assistants.html',row = u,username = current_user.username,image = image)
+    else:
+        flash("Assistant is not assigned")
+        return render_template('Doctor/doctor_sites/current_assistants.html',row = u,username = current_user.username)
+
+@doctor_routes_bp.route('/doctor/past_assistants')
+def past_assistant():
+    u = past_user_role.query.filter_by(role = "assistant", doctor_username = current_user.username).all()
+    images = []
+    for i in u:
+        images.append(base64.b64encode(i.File).decode('ascii'))
+    if len(u) == 0:
+        flash("No past assistants")
+    return render_template('Doctor/doctor_sites/past_assistants.html',users = u,username = current_user.username,images = images)
